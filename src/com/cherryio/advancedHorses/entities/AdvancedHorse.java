13864c1973edc92b7f6cef91752cf74adafc7937
Proxying <kieran_quigley_CFC@hotmail.com>
2016-03-29 03:13:23 +0100

    Gee god that was awkward. Override NMS code for some stuff on the entity to make it lose hunger/thirst every so often. Player can now feed/water them correctly.

--- src/com/cherryio/advancedHorses/entities/AdvancedHorse.java
+++ src/com/cherryio/advancedHorses/entities/AdvancedHorse.java
@@ -1,34 +1,23 @@
 package com.cherryio.advancedHorses.entities;
 
-import net.minecraft.server.v1_9_R1.EntityHorse;
-import net.minecraft.server.v1_9_R1.GenericAttributes;
-import net.minecraft.server.v1_9_R1.World;
+import com.cherryio.advancedHorses.utils.Config;
+import net.minecraft.server.v1_9_R1.*;
+import org.bukkit.event.entity.EntityRegainHealthEvent;
 
 import java.util.Random;
+import java.util.UUID;
 
 /**
  * Created by Kieran on 26-Mar-16.
  */
 public class AdvancedHorse extends EntityHorse {
 
-    public boolean isNeutered() {
-        return isNeutered;
-    }
-
-    public int getHorseGender() {
-        return horseGender;
-    }
-
-    public void setHorseGender(int horseGender) {
-        this.horseGender = horseGender;
-    }
-
-    public void setNeutered(boolean neutered) {
-        isNeutered = neutered;
-    }
-
     private int horseGender; //0 Female, 1 Male
     private boolean isNeutered; //false No, true Yes
+    private int hungerLevel;
+    private int hydrationLevel;
+    private int bO;
+    private int bV;
 
     public AdvancedHorse(World world, int horseGender, boolean isNeutered) {
         super(world);
@@ -42,9 +31,271 @@ public class AdvancedHorse extends EntityHorse {
         this.getAttributeInstance(attributeJumpStrength).setValue(jump);
         this.getAttributeInstance(GenericAttributes.maxHealth).setValue((new Random().nextInt(16) + 15));
         this.setHealth((float) this.getAttributeInstance(GenericAttributes.maxHealth).getValue());
+        this.setHungerLevel(500);
+        this.setHydrationLevel(500);
     }
 
     public AdvancedHorse(World world) {
         super(world);
+    }
+
+    public boolean isNeutered() {
+        return isNeutered;
+    }
+
+    public void setNeutered(boolean neutered) {
+        isNeutered = neutered;
+    }
+
+    public int getHorseGender() {
+        return horseGender;
+    }
+
+    public void setHorseGender(int horseGender) {
+        this.horseGender = horseGender;
+    }
+
+    public int getHungerLevel() {
+        return hungerLevel;
+    }
+
+    private void setHungerLevel(int hungerLevel) {
+        if (hungerLevel > 0 && hungerLevel < 501) {
+            this.hungerLevel = hungerLevel;
+        } else if (hungerLevel < 0) {
+            this.hungerLevel = 0;
+        } else if (hungerLevel > 500) {
+            this.hungerLevel = 500;
+        }
+    }
+
+    public int getHydrationLevel() {
+        return hydrationLevel;
+    }
+
+    private void setHydrationLevel(int hydrationLevel) {
+        if (hydrationLevel > 0 && hydrationLevel < 501) {
+            this.hydrationLevel = hydrationLevel;
+        } else if (hydrationLevel < 0) {
+            this.hydrationLevel = 0;
+        } else if (hydrationLevel > 500) {
+            this.hydrationLevel = 500;
+        }
+    }
+
+    public double getHorseSpeed() {
+        return this.getAttributeInstance(GenericAttributes.MOVEMENT_SPEED).getValue();
+    }
+
+    public void setSpeed(double avgParentSpeed) {
+        this.getAttributeInstance(GenericAttributes.MOVEMENT_SPEED).setValue(avgParentSpeed);
+    }
+
+    public void setMaxHP(double avgParentHP) {
+        this.getAttributeInstance(GenericAttributes.maxHealth).setValue(avgParentHP);
+    }
+
+    public void setJump(double avgParentJump) {
+        this.getAttributeInstance(attributeJumpStrength).setValue(avgParentJump);
+    }
+
+    @Override
+    public void n() {
+        if(this.random.nextInt(200) == 0) {
+            this.bv = 1;
+        }
+
+        super.n();
+        if(!this.world.isClientSide) {
+            if (this.isTamed()) {
+                if (this.random.nextInt(2000) == 0) {
+                    if (this.isVehicle()) {
+                        this.setHydrationLevel(this.getHydrationLevel() - 2);
+                        if (this.getHydrationLevel() <= 50) {
+                            this.setSpeed(this.getHorseSpeed() / 2);
+                            this.setJump(this.getJumpStrength() / 2);
+                        }
+                    } else {
+                        this.setHydrationLevel(this.getHydrationLevel() - 1);
+                        if (this.getHydrationLevel() <= 50) {
+                            this.setSpeed(this.getHorseSpeed() / 2);
+                            this.setJump(this.getJumpStrength() / 2);
+                        }
+                    }
+                }
+                if (this.random.nextInt(1500) == 0) {
+                    if (this.isVehicle()) {
+                        this.setHungerLevel(this.getHydrationLevel() - 2);
+                        if (this.getHungerLevel() <= 0) {
+                            if (new Config<Boolean>("settings.horseDeathNoFood").getValue()) {
+                                this.die();
+                            }
+                        }
+                    } else {
+                        this.setHungerLevel(this.getHydrationLevel() - 1);
+                        if (this.getHungerLevel() <= 0) {
+                            if (new Config<Boolean>("settings.horseDeathNoFood").getValue()) {
+                                this.die();
+                            }
+                        }
+                    }
+                }
+            }
+
+            if(this.random.nextInt(900) == 0 && this.deathTicks == 0) {
+                this.heal(1.0F, EntityRegainHealthEvent.RegainReason.REGEN);
+            }
+
+            if(!this.dm() && !this.isVehicle() && this.random.nextInt(300) == 0 && this.world.getType(new BlockPosition(MathHelper.floor(this.locX), MathHelper.floor(this.locY) - 1, MathHelper.floor(this.locZ))).getBlock() == Blocks.GRASS) {
+                this.u(true);
+            }
+
+            if(this.dm() && ++this.bO > 50) {
+                this.bO = 0;
+                this.u(false);
+            }
+
+            if(this.do_() && !this.db() && !this.dm()) {
+                EntityHorse entityhorse = this.a(this, 16.0D);
+                if(entityhorse != null && this.h(entityhorse) > 4.0D) {
+                    this.navigation.a(entityhorse);
+                }
+            }
+
+            if(this.dG() && this.bV++ >= 18000) {
+                this.die();
+            }
+        }
+    }
+
+    @Override
+    public void b(NBTTagCompound nbttagcompound) {
+        super.b(nbttagcompound);
+        nbttagcompound.setBoolean("EatingHaystack", this.dm());
+        nbttagcompound.setBoolean("ChestedHorse", this.hasChest());
+        nbttagcompound.setBoolean("HasReproduced", this.hasReproduced());
+        nbttagcompound.setBoolean("Bred", this.do_());
+        nbttagcompound.setInt("Type", this.getType().k());
+        nbttagcompound.setInt("Variant", this.getVariant());
+        nbttagcompound.setInt("Temper", this.getTemper());
+        nbttagcompound.setBoolean("Tame", this.isTamed());
+        nbttagcompound.setBoolean("SkeletonTrap", this.dG());
+        nbttagcompound.setInt("SkeletonTrapTime", this.bV);
+        if(this.getOwnerUUID() != null) {
+            nbttagcompound.setString("OwnerUUID", this.getOwnerUUID().toString());
+        }
+
+        nbttagcompound.setInt("Bukkit.MaxDomestication", this.maxDomestication);
+        if(this.hasChest()) {
+            NBTTagList nbttaglist = new NBTTagList();
+
+            for(int i = 2; i < this.inventoryChest.getSize(); ++i) {
+                ItemStack itemstack = this.inventoryChest.getItem(i);
+                if(itemstack != null) {
+                    NBTTagCompound nbttagcompound1 = new NBTTagCompound();
+                    nbttagcompound1.setByte("Slot", (byte)i);
+                    itemstack.save(nbttagcompound1);
+                    nbttaglist.add(nbttagcompound1);
+                }
+            }
+
+            nbttagcompound.set("Items", nbttaglist);
+        }
+
+        if(this.inventoryChest.getItem(1) != null) {
+            nbttagcompound.set("ArmorItem", this.inventoryChest.getItem(1).save(new NBTTagCompound()));
+        }
+
+        if(this.inventoryChest.getItem(0) != null) {
+            nbttagcompound.set("SaddleItem", this.inventoryChest.getItem(0).save(new NBTTagCompound()));
+        }
+
+    }
+
+    @Override
+    public void a(NBTTagCompound nbttagcompound) {
+        super.a(nbttagcompound);
+        this.u(nbttagcompound.getBoolean("EatingHaystack"));
+        this.q(nbttagcompound.getBoolean("Bred"));
+        this.setHasChest(nbttagcompound.getBoolean("ChestedHorse"));
+        this.s(nbttagcompound.getBoolean("HasReproduced"));
+        this.setType(EnumHorseType.a(nbttagcompound.getInt("Type")));
+        this.setVariant(nbttagcompound.getInt("Variant"));
+        this.setTemper(nbttagcompound.getInt("Temper"));
+        this.setTame(nbttagcompound.getBoolean("Tame"));
+        this.x(nbttagcompound.getBoolean("SkeletonTrap"));
+        this.bV = nbttagcompound.getInt("SkeletonTrapTime");
+        String s;
+        if(nbttagcompound.hasKeyOfType("OwnerUUID", 8)) {
+            s = nbttagcompound.getString("OwnerUUID");
+        } else {
+            String attributeinstance = nbttagcompound.getString("Owner");
+            s = NameReferencingFileConverter.a(this.h(), attributeinstance);
+        }
+
+        if(!s.isEmpty()) {
+            this.setOwnerUUID(UUID.fromString(s));
+        }
+
+        if(nbttagcompound.hasKey("Bukkit.MaxDomestication")) {
+            this.maxDomestication = nbttagcompound.getInt("Bukkit.MaxDomestication");
+        }
+
+        AttributeInstance var8 = this.getAttributeMap().a("Speed");
+        if(var8 != null) {
+            this.getAttributeInstance(GenericAttributes.MOVEMENT_SPEED).setValue(var8.b() * 0.25D);
+        }
+
+        if(this.hasChest()) {
+            NBTTagList itemstack = nbttagcompound.getList("Items", 10);
+            this.loadChest();
+
+            for(int i = 0; i < itemstack.size(); ++i) {
+                NBTTagCompound nbttagcompound1 = itemstack.get(i);
+                int j = nbttagcompound1.getByte("Slot") & 255;
+                if(j >= 2 && j < this.inventoryChest.getSize()) {
+                    this.inventoryChest.setItem(j, ItemStack.createStack(nbttagcompound1));
+                }
+            }
+        }
+
+        ItemStack var9;
+        if(nbttagcompound.hasKeyOfType("ArmorItem", 10)) {
+            var9 = ItemStack.createStack(nbttagcompound.getCompound("ArmorItem"));
+            if(var9 != null && EnumHorseArmor.b(var9.getItem())) {
+                this.inventoryChest.setItem(1, var9);
+            }
+        }
+
+        if(nbttagcompound.hasKeyOfType("SaddleItem", 10)) {
+            var9 = ItemStack.createStack(nbttagcompound.getCompound("SaddleItem"));
+            if(var9 != null && var9.getItem() == Items.SADDLE) {
+                this.inventoryChest.setItem(0, var9);
+            }
+        }
+        this.dK();
+    }
+
+    private void dK() {
+        if(!this.world.isClientSide) {
+            this.t(this.inventoryChest.getItem(0) != null);
+            if(this.getType().j()) {
+                this.f(this.inventoryChest.getItem(1));
+            }
+        }
+
+    }
+
+    public void feedHorse() {
+        this.setHungerLevel(this.getHungerLevel() + 50);
+    }
+
+    public void waterHorse() {
+        final int prevLevel = this.getHydrationLevel();
+        this.setHydrationLevel(this.getHydrationLevel() + 50);
+        if (this.getHydrationLevel() > 50 && prevLevel <= 50) {
+            this.setSpeed(this.getHorseSpeed() * 2);
+            this.setJump(this.getJumpStrength() * 2);
+        }
     }
 }
--- src/com/cherryio/advancedHorses/listeners/AdvancedHorsesListener.java
+++ src/com/cherryio/advancedHorses/listeners/AdvancedHorsesListener.java
@@ -5,6 +5,7 @@ import com.cherryio.advancedHorses.utils.Config;
 import net.minecraft.server.v1_9_R1.EnumHorseType;
 import net.minecraft.server.v1_9_R1.GenericAttributes;
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.craftbukkit.v1_9_R1.CraftWorld;
@@ -46,6 +47,7 @@ public class AdvancedHorsesListener implements Listener {
         event.setDamage(0);
         event.setCancelled(true);
         AdvancedHorse advancedHorse1 = (AdvancedHorse) ((CraftLivingEntity) horse).getHandle();
+        player.sendMessage(ChatColor.GRAY + "---------------------------");
         if (horse.getCustomName() == null || horse.getCustomName().isEmpty()) {
             player.sendMessage(ChatColor.YELLOW + "Name" + ChatColor.GRAY + ": " + ChatColor.AQUA + "None");
         } else {
@@ -74,6 +76,8 @@ public class AdvancedHorsesListener implements Listener {
         player.sendMessage(ChatColor.YELLOW + "Jump Height (Blocks)" + ChatColor.GRAY + ": " + ChatColor.AQUA + blockJumpHeight);
         player.sendMessage(ChatColor.YELLOW + "Speed (Blocks/Second)" + ChatColor.GRAY + ": " + ChatColor.AQUA + blockPerSecondSpeed);
         player.sendMessage(ChatColor.YELLOW + "Health" + ChatColor.GRAY + ": " + ChatColor.AQUA + Math.round(horse.getHealth()) + ChatColor.GRAY + "/" + ChatColor.AQUA + Math.round(horse.getMaxHealth()));
+        player.sendMessage(ChatColor.YELLOW + "Hunger" + ChatColor.GRAY + ": " + ChatColor.AQUA + advancedHorse1.getHungerLevel() + ChatColor.GRAY + "/" + ChatColor.AQUA + "500");
+        player.sendMessage(ChatColor.YELLOW + "Hydration" + ChatColor.GRAY + ": " + ChatColor.AQUA + advancedHorse1.getHydrationLevel() + ChatColor.GRAY + "/" + ChatColor.AQUA + "500");
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
@@ -106,6 +110,7 @@ public class AdvancedHorsesListener implements Listener {
     public void horseSpawn(CreatureSpawnEvent event) {
         if (event.getEntity().getType() != EntityType.HORSE) return;
         if (event.getSpawnReason() == CreatureSpawnEvent.SpawnReason.CUSTOM) return;
+        if (event.getSpawnReason() == CreatureSpawnEvent.SpawnReason.BREEDING) return;
         final Entity horse = event.getEntity();
         if (!event.getEntity().isDead()) {
             event.getEntity().remove();
@@ -126,11 +131,12 @@ public class AdvancedHorsesListener implements Listener {
     public void horseMate(CreatureSpawnEvent event) {
         if (event.getSpawnReason() != CreatureSpawnEvent.SpawnReason.BREEDING) return;
         if (event.getEntity().getType() != EntityType.HORSE) return;
-        Horse babyHorse = (Horse) event.getEntity();
+        final Horse babyHorse = (Horse) event.getEntity();
         boolean found = false;
         boolean found2 = false;
         Horse parent1 = babyHorse;
         Horse parent2 = babyHorse;
+        boolean shouldSpawn = true;
         for (int i = 0; i < 2; i++) {
             List<Entity> entities = babyHorse.getNearbyEntities(i, 0, i);
             for (Entity e : entities) {
@@ -149,21 +155,38 @@ public class AdvancedHorsesListener implements Listener {
             }
             if (found2) break;
         }
+        event.getEntity().remove();
         if (found && found2) {
             AdvancedHorse advancedHorse1 = (AdvancedHorse) ((CraftLivingEntity) parent1).getHandle();
             AdvancedHorse advancedHorse2 = (AdvancedHorse) ((CraftLivingEntity) parent2).getHandle();
             if (advancedHorse1.isNeutered() || advancedHorse2.isNeutered() ) {
-                babyHorse.remove();
-                return;
+                shouldSpawn = false;
             }
             if (advancedHorse1.getHorseGender() == 1) {
                 if (advancedHorse1.getHorseGender() == 1) {
-                    babyHorse.remove();
+                    shouldSpawn = false;
                 }
             } else if (advancedHorse1.getHorseGender() == 0) {
                 if (advancedHorse1.getHorseGender() == 0) {
-                    babyHorse.remove();
+                    shouldSpawn = false;
                 }
+            }
+            if (shouldSpawn) {
+                int gender;
+                if (new Random().nextInt(99) < new Config<Integer>("settings.maleHorseOnBirthChance").getValue()) {
+                    gender = 1;
+                } else {
+                    gender = 0;
+                }
+                AdvancedHorse advancedHorse = new AdvancedHorse(((CraftWorld) event.getEntity().getWorld()).getHandle(), gender, false);
+                advancedHorse.setLocation(babyHorse.getLocation().getX(), babyHorse.getLocation().getY(), babyHorse.getLocation().getZ(), 0 ,0);
+                ((CraftWorld) event.getEntity().getWorld()).getHandle().addEntity(advancedHorse, CreatureSpawnEvent.SpawnReason.CUSTOM);
+                advancedHorse.setLocation(babyHorse.getLocation().getX(), babyHorse.getLocation().getY(), babyHorse.getLocation().getZ(), 0 ,0);
+                advancedHorse.setMaxHP((advancedHorse1.getMaxHealth() + advancedHorse2.getMaxHealth()) / 1.9);
+                advancedHorse.setJump((advancedHorse1.getJumpStrength() + advancedHorse2.getJumpStrength()) / 1.9);
+                advancedHorse.setSpeed((advancedHorse1.getHorseSpeed() + advancedHorse2.getHorseSpeed()) / 1.9);
+                org.bukkit.entity.Horse horse = (org.bukkit.entity.Horse) advancedHorse.getBukkitEntity();
+                horse.setBaby();
             }
         }
     }
@@ -219,5 +242,65 @@ public class AdvancedHorsesListener implements Listener {
                 player.getItemInHand().setType(Material.AIR);
             }
         }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void waterHorse(EntityDamageByEntityEvent event) {
+        if (!(event.getDamager() instanceof Player)) return;
+        if (!(event.getEntity() instanceof Horse)) return;
+        Player player = (Player) event.getDamager();
+        if (player.getItemInHand() == null || player.getItemInHand().getType() != Material.WATER_BUCKET) return;
+        Horse horse = (Horse) event.getEntity();
+        if (!horse.isTamed()) {
+            event.setCancelled(true);
+            event.setDamage(0);
+            player.sendMessage(ChatColor.RED + "You cannot water wild horses!");
+            return;
+        }
+        event.setDamage(0);
+        event.setCancelled(true);
+        AdvancedHorse advancedHorse1 = (AdvancedHorse) ((CraftLivingEntity) horse).getHandle();
+        if (advancedHorse1.getHydrationLevel() < 500) {
+            player.sendMessage(ChatColor.GREEN + "You have sated this Horses thirst!");
+            advancedHorse1.waterHorse();
+            if (player.getGameMode() != GameMode.CREATIVE) {
+                player.setItemInHand(new ItemStack(Material.BUCKET, 1));
+            }
+        } else {
+            player.sendMessage(ChatColor.RED + "This horse is not thirsty!");
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void feedHorse(EntityDamageByEntityEvent event) {
+        if (!(event.getDamager() instanceof Player)) return;
+        if (!(event.getEntity() instanceof Horse)) return;
+        Player player = (Player) event.getDamager();
+        if (player.getItemInHand() == null) return;
+        if (player.getItemInHand().getType() != Material.WHEAT && player.getItemInHand().getType() != Material.APPLE && player.getItemInHand().getType() != Material.HAY_BLOCK && player.getItemInHand().getType() != Material.SUGAR) return;
+        Horse horse = (Horse) event.getEntity();
+        if (!horse.isTamed()) {
+            event.setCancelled(true);
+            event.setDamage(0);
+            player.sendMessage(ChatColor.RED + "You cannot feed wild horses!");
+            return;
+        }
+        event.setDamage(0);
+        event.setCancelled(true);
+        AdvancedHorse advancedHorse1 = (AdvancedHorse) ((CraftLivingEntity) horse).getHandle();
+        if (advancedHorse1.getHungerLevel() < 500) {
+            player.sendMessage(ChatColor.GREEN + "You have sated this Horses thirst!");
+            advancedHorse1.feedHorse();
+            if (player.getGameMode() != GameMode.CREATIVE) {
+                if (player.getItemInHand().getAmount() > 1) {
+                    player.getItemInHand().setAmount(player.getItemInHand().getAmount() - 1);
+                } else {
+                    player.setItemInHand(new ItemStack(Material.AIR));
+                }
+            }
+        } else {
+            player.sendMessage(ChatColor.RED + "This horse is not hungry!");
+        }
+
     }
 }
